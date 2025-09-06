2b0ee67b34182407f1b7105955555744d2a5eaaf
Andrea Klost <anklost@siue.edu>
2025-09-06 01:59:01 -0500

    Apples now automatically delete themselves

---
+++ Assets/Scripts/Apple.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Apple : MonoBehaviour {
+    // Height where apples are removed
+    public static float BottomY = -20f;
+    
+    void Update()
+    {
+        // Remove apples that fall off the screen
+        if (transform.position.y < BottomY) {
+            Destroy(this.gameObject);
+        }
+    }
+}
